23aedce8aeb38b94d781c11dbd5728396b6eb9f5
Rasmus Steen <rasmus@steen.ninja>
2026-06-10 22:48:35 +0200

    try to build mental model

---
+++ Kofoten.SimpleCli.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Kofoten.SimpleCli.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static ServiceCollection AddCliCommands(this ServiceCollection services, string[] args, Action<CliCommandRouter> configure)
+    {
+        var router = new CliCommandRouter();
+        configure(router);
+
+        var command = router.GetCommand(args);
+        services.TryAddSingleton<ICliCommand>(factory);
+        return services;
+    }
+}
---
+++ Kofoten.SimpleCli/CliCommandRouter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kofoten.SimpleCli;
+
+public class CliCommandRouter
+{
+    private Dictionary<string, Func<ArraySegment<string>, ICliCommand>> factories = [];
+
+    public ICliCommand GetCommand(string[] args) => ResolveCommand(new ArraySegment<string>(args));
+
+    public void Map(string verb, Action<CliCommandRouter> configure)
+    {
+        var router = new CliCommandRouter();
+        configure(router);
+        factories.Add(verb, router.GetCommand);
+    }
+
+    public void Map(string verb, Func<string[], ICliCommand> factory)
+    {
+        factories.Add(verb, factory);
+    }
+
+    private ICliCommand ResolveCommand(ArraySegment<string> args)
+    {
+        if (args.Count == 0)
+        {
+            throw new ArgumentException($"Command '{{command}}' requires at least one argument");
+        }
+
+        var verb = args.Array[args.Offset];
+        if (factories.TryGetValue(verb, out var factory))
+        {
+            var subsegment = new ArraySegment<string>(args.Array, args.Offset + 1, args.Count - 1);
+            return factory(subsegment);
+        }
+
+        throw new ArgumentException($"Invalid verb: {verb}");
+    }
+}
